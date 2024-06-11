12625befa3394cae63b731c2721f5040bd7af61f
N8MARERahul <rahulpal521843@gmail.com>
2024-06-11 22:20:35 +0530

    add comment , like, playlist and tweet models

---
+++ src/models/playlist.model.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const playListSchema = new Schema(
+    {
+        name: {
+            type : String,
+            required: true
+        },
+        description: {
+            type : String
+        },
+        videos : [{
+            type : Schema.Types.ObjectId,
+            ref : "Video"
+        }],
+        owner : {
+            type : Schema.Types.ObjectId,
+            ref : "User"
+        } 
+    },
+    {
+        timestamps: true,
+    }
+)
+
+playListSchema.plugin(mongooseAggregatePaginate)
+
+export const PlayList = mongoose.model("PlayList", playListSchema)
